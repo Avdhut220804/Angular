b52edff757dec2aa18448f22af1e15da9b0b768c
Avdhut <aspanhale22@gmail.com>
2025-03-05 11:37:00 +0530

    Initial commit

---
+++ WAD_Angular/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+
+export const routes: Routes = [
+    {path: 'signup', component:SignupComponent},
+    {path: 'signin', component: SigninComponent},
+    {path: '', redirectTo: 'signin', pathMatch: 'full'}
+];
